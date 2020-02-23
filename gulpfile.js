87a15bd8ae7112d704d43695d5edcae819be85d0
Wojtec <poninski.w@gmail.com>
2020-02-23 17:53:49 +0100

    suffix working

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ let gulp = require('gulp');
 let ts = require('gulp-typescript');
 let tsProject = ts.createProject('tsconfig.json');
 let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
+let cleanCss = require('gulp-clean-css');
+let rename = require('gulp-rename');
 
 sass.compiler = require('node-sass');
 
@@ -17,3 +20,19 @@ gulp.task('sass', ()=>{
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('dist'));
 });
+
+gulp.task('auto', () =>{
+  return gulp.src('dist/test.css')
+  .pipe(autoprefixer({
+      cascade: false
+  }))
+  .pipe(gulp.dest('autopre'))
+
+});
+
+gulp.task('minify-css', ()=>{
+  return gulp.src('autopre/test.css')
+  .pipe(cleanCss({compability: 'ie8'}))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('dist'));
+})
